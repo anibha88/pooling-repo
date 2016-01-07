494a0c3cc167326179e18044520e292283f7b9df
anibha88 <anibha88@gmail.com>
2016-01-07 11:38:35 +0530

    test commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
 
   config.assets.compile = true
   # config.assets.precompile += %w( *.js *.css *.eot *.woff *.ttf *.svg *.otf )
-  config.assets.precompile =  ['.js', '.css', '*.css.erb']
+  # config.assets.precompile =  ['.js', '.css', '*.css.erb','.eot', '.woff', '.ttf', '.svg', '.otf']
   config.assets.precompile += %w(active_admin.css active_admin/print.css rsvp.css)
   config.assets.precompile += %w(active_admin.js global.js rsvp.js)
   # Eager load code on boot. This eager loads most of Rails and
@@ -34,7 +34,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # config.assets.compile = false
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
